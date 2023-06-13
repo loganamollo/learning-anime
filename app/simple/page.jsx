1062a0402189fae6904de17846dc205dd294c46e
loganamollo <loganamollo@gmail.com>
2023-06-13 13:22:09 +0300

    added orbit controls

--- app/simple/page.jsx
+++ app/simple/page.jsx
@@ -1,11 +1,13 @@
 'use client'
 import Box from "@/components/objects/Box"
+import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 
 export default function Page() {
     return (
         <main className="bg-red-200 w-full h-[100vh]">
             <Canvas className="h-full w-full bg-gray-300">
+                <OrbitControls />
                 <Box />
             </Canvas>
         </main>
